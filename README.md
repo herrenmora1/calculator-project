43cdc0d725c9685e28e2b34a2bc80a9a46d2ae6b
Jack Chin <jkchin2@buffalo.edu>
2023-07-22 19:23:36 -0400

    PYSIMPLEGUI IS DOOKIE >:( SO NOW I GUESS I'M REDOING THIS BUT IN TKINTER NOW :( BUT IT DOESN'T SEEM SO BAD

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # calculator-project
-calculator project ? THE JACKULATOR >:)
-writing using pythongui, python wrapper of tkinter
+calculator project ? THE JACKULATOR >:) REVISED VERSION
+F PYSIMPLEGUI THIS THING AINT SIMPLE AT ALL AND ITS DOOKIE WATER >:(
--- main.py
+++ main.py
@@ -1,35 +1,47 @@
 import PySimpleGUI as sg 
+import tkinter as tk
 
-total = 0
-
-layout = [[sg.Text(text='0', size=(15,2), key='number', justification='right')], 
-          [sg.Button("⌫", size=(2,2)), sg.Button("C", size=(2,2), ), sg.Button("CE", size=(2,2)), sg.Button("/", size=(2,2))],
-          [sg.Button('7', size=(2,2)), sg.Button('8', size=(2,2)), sg.Button('9', size=(2,2)), sg.Button("x", size=(2,2))],
-          [sg.Button('4', size=(2,2)), sg.Button('5', size=(2,2)), sg.Button('6', size=(2,2)), sg.Button("-", size=(2,2))],
-          [sg.Button('1', size=(2,2)), sg.Button('2', size=(2,2)), sg.Button('3', size=(2,2)), sg.Button("+", size=(2,2))],
-          [sg.Button("+/-", size=(2,2)), sg.Button('0', size=(2,2)), sg.Button(".", size=(2,2)), sg.Button("=", size=(2,2))]]
+###################################### YEAH PYSIMPLEGUI IS GARBO DOOKIE WATER NAH I AINT TAKING IT BACK ######################################
+# total = 0
+# layout = [[sg.Text(text='0', size=(15,2), key='number', justification='right')], 
+        #   [sg.Button("⌫", size=(2,2)), sg.Button("C", size=(2,2), ), sg.Button("CE", size=(2,2)), sg.Button("/", size=(2,2))],
+        #   [sg.Button('7', size=(2,2)), sg.Button('8', size=(2,2)), sg.Button('9', size=(2,2)), sg.Button("x", size=(2,2))],
+        #   [sg.Button('4', size=(2,2)), sg.Button('5', size=(2,2)), sg.Button('6', size=(2,2)), sg.Button("-", size=(2,2))],
+        #   [sg.Button('1', size=(2,2)), sg.Button('2', size=(2,2)), sg.Button('3', size=(2,2)), sg.Button("+", size=(2,2))],
+        #   [sg.Button("+/-", size=(2,2)), sg.Button('0', size=(2,2)), sg.Button(".", size=(2,2)), sg.Button("=", size=(2,2))]]
 
 #creates window 
-window = sg.Window(title="Jackulator", layout=layout, margins=(20, 25), auto_size_text=True, resizable=True, finalize=True)
+# window = sg.Window(title="Jackulator", layout=layout, margins=(20, 25), auto_size_text=True, resizable=True, finalize=True)
 #minimum size the window can be shrunk to (specific to my pc)
-window.set_min_size((180, 350))
+# window.set_min_size((180, 350))
 
 #event loop 
-while True: 
-    event, values = window.read()
-    #End program if user closes window
-    if event == sg.WIN_CLOSED:
-        break
+# while True: 
+    # event, values = window.read()
+    # End program if user closes window
+    # if event == sg.WIN_CLOSED:
+        # break
 
     #number input
-    if event == '0' or event == '1' or event == '2' or event == '3' or event == '4' or event == '5' or event == '6' or event == '7' or event == '8' or event == '9':
-        total = int(str(total) + sg.Button(event).get_text())
-        window['number'].update(total) 
+    # if event == '0' or event == '1' or event == '2' or event == '3' or event == '4' or event == '5' or event == '6' or event == '7' or event == '8' or event == '9':
+        # total = int(str(total) + sg.Button(event).get_text())
+        # window['number'].update(total) 
     
     #invert the number
-    if event == "+/-":
-        total *= -1
-        window['number'].update(total)
+    # if event == "+/-":
+        # total *= -1
+        # window['number'].update(total)
 
-window.close()
+# window.close()
+
+###################################### END OF DOOKIE WATER PYSIMPLEGUI ######################################
+
+root = tk.Tk() #WINDOW
+
+#TODO: PUT THINGS IN HERE
+message = tk.Label(root, text="Hello world!") #creates message widget
+message.pack() #shows the widget
+
+#keep the window displaying
+root.mainloop()
 
