b5b816cc02fd0475a92ca14e9fdb8ad386fd87b5
Jack Chin <jkchin2@buffalo.edu>
2023-07-23 20:01:54 -0400

    i copied some files from the internet and also made significant progress on my buttons and stuff

--- main.py
+++ main.py
@@ -1,98 +1,118 @@
-import PySimpleGUI as sg 
 import tkinter as tk
 
-def seven_pressed(event):
-    print('7')
+#point out global total variable
+total = 0
+
+#methods for each button press ? 
+
+def seven_pressed():
+    global total 
     total = int(str(total) + '7')
+    message["text"] = total 
 
-###################################### YEAH PYSIMPLEGUI IS GARBO DOOKIE WATER NAH I AINT TAKING IT BACK ######################################
-# total = 0
-# layout = [[sg.Text(text='0', size=(15,2), key='number', justification='right')], 
-        #   [sg.Button("⌫", size=(2,2)), sg.Button("C", size=(2,2), ), sg.Button("CE", size=(2,2)), sg.Button("/", size=(2,2))],
-        #   [sg.Button('7', size=(2,2)), sg.Button('8', size=(2,2)), sg.Button('9', size=(2,2)), sg.Button("x", size=(2,2))],
-        #   [sg.Button('4', size=(2,2)), sg.Button('5', size=(2,2)), sg.Button('6', size=(2,2)), sg.Button("-", size=(2,2))],
-        #   [sg.Button('1', size=(2,2)), sg.Button('2', size=(2,2)), sg.Button('3', size=(2,2)), sg.Button("+", size=(2,2))],
-        #   [sg.Button("+/-", size=(2,2)), sg.Button('0', size=(2,2)), sg.Button(".", size=(2,2)), sg.Button("=", size=(2,2))]]
-
-#creates window 
-# window = sg.Window(title="Jackulator", layout=layout, margins=(20, 25), auto_size_text=True, resizable=True, finalize=True)
-#minimum size the window can be shrunk to (specific to my pc)
-# window.set_min_size((180, 350))
-
-#event loop 
-# while True: 
-    # event, values = window.read()
-    # End program if user closes window
-    # if event == sg.WIN_CLOSED:
-        # break
-
-    #number input
-    # if event == '0' or event == '1' or event == '2' or event == '3' or event == '4' or event == '5' or event == '6' or event == '7' or event == '8' or event == '9':
-        # total = int(str(total) + sg.Button(event).get_text())
-        # window['number'].update(total) 
+def eight_pressed():
+    global total 
+    total = int(str(total) + '8')
+    message["text"] = total 
     
-    #invert the number
-    # if event == "+/-":
-        # total *= -1
-        # window['number'].update(total)
+def nine_pressed():
+    global total 
+    total = int(str(total) + '9')
+    message["text"] = total 
 
-# window.close()
+def four_pressed():
+    global total 
+    total = int(str(total) + '4')
+    message["text"] = total 
 
-###################################### END OF DOOKIE WATER PYSIMPLEGUI ######################################
+def five_pressed():
+    global total 
+    total = int(str(total) + '5')
+    message["text"] = total 
+
+def six_pressed():
+    global total 
+    total = int(str(total) + '6')
+    message["text"] = total 
+
+def one_pressed():
+    global total 
+    total = int(str(total) + '1')
+    message["text"] = total 
+
+def two_pressed():
+    global total 
+    total = int(str(total) + '2')
+    message["text"] = total 
+
+def three_pressed():
+    global total 
+    total = int(str(total) + '3')
+    message["text"] = total 
+
+def zero_pressed():
+    global total 
+    total = int(str(total) + '0')
+    message["text"] = total 
 
 root = tk.Tk() #WINDOW
 root.title("JACKULATOR REVISED V2")
 
-####### window specifications #######
-
+######! window specifications !######
+#window's rows and columns 
+root.rowconfigure([0,1,2,3,4,5], minsize=50, weight=1)
+root.columnconfigure([0,1,2,3], minsize=50, weight=1)
 #window size 
 window_width = 360
 window_height = 500
-
 #declare minimum window size 
 root.minsize(360, 500)
-
 #get screen dimension 
 screen_width = root.winfo_screenwidth()
 screen_height = root.winfo_screenheight()
-
 #find center of screen
 center_x = int(screen_width/2 - window_width / 2)
 center_y = int(screen_height/2 - window_height / 2)
-
 #set window to center of screen
 root.geometry(f'{window_width}x{window_height}+{center_x}+{center_y}')
+######! end of window specifications !######
 
-####### end of window specifications #######
+#TODO: PUT BUTTON/OPERATION WIDGETS IN HERE
 
+message = tk.Label(master=root, text=total) #creates message widget
+message.grid(row=0,column=0, columnspan=4)
 
+#? NUMBER BUTTON SECTION ?#
 
+#ROW 2
+seven = tk.Button(master=root, text='7', command=seven_pressed)
+seven.grid(row=2,column=0,sticky="nsew") #remove sticky to stop it from being a big box
 
+eight = tk.Button(master=root, text='8', command=eight_pressed)
+eight.grid(row=2,column=1,sticky="nsew")
 
+nine = tk.Button(master=root, text='9', command=nine_pressed)
+nine.grid(row=2,column=2,sticky="nsew")
 
-#point out global total variable
-total = '0' 
+#ROW 3
+four = tk.Button(master=root, text='4', command=four_pressed)
+four.grid(row=3,column=0,sticky="nsew")
 
-#methods for each button press ? 
+five = tk.Button(master=root, text='5', command=five_pressed)
+five.grid(row=3,column=1,sticky="nsew")
 
-def numpress(num):
-    global total 
-    total += num
-    displaytotal.set(total)
+six = tk.Button(master=root, text='6', command=six_pressed)
+six.grid(row=3,column=2,sticky="nsew")
 
-#TODO: PUT WIDGETS IN HERE
+#ROW 4
+one = tk.Button(master=root, text='1', command=one_pressed)
+one.grid(row=4,column=0,sticky="nsew")
 
-displaytotal = tk.StringVar()
+two = tk.Button(master=root, text='2', command=two_pressed)
+two.grid(row=4,column=1,sticky="nsew")
 
-message = tk.Label(root, textvariable=displaytotal) #creates message widget
-message.pack() #shows the widget
-
-#attempt at a button doesn't work yet 
-seven = tk.Button(root, text=7)
-seven.bind('<Button>', )
-seven.focus()
-seven.pack()
-#end of button attempt 
+three = tk.Button(master=root, text='3', command=three_pressed)
+three.grid(row=4,column=2,sticky="nsew")
 
 #keep the window displaying
 root.mainloop()
